f3ebb26a1df48efaf8491e1d56430d8292b2a60b
MMMMMiann <145739157+MMMMMiann@users.noreply.github.com>
2023-10-27 23:21:28 +1100

    Update sketch.js
    up

--- sketch.js
+++ sketch.js
@@ -1,7 +1,67 @@
+let song;
+let fft;
+
+function preload() {
+  song = loadSound('assets/sample-visualisation.mp3')
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  fft = new p5.FFT();
 }
 
 function draw() {
-  background(220);
+  if (getAudioContext().state !== 'running') {
+    background(220);
+    text('tap here to play some sound!', 10, 20, width - 20);
+    return;
+    }
+
+  background(225);
+  stroke(0);
+  strokeWeight(1);
+
+  translate(width / 2, height / 2);
+
+  let wave = fft.waveform();
+
+  beginShape()
+  for(let i = 0; i<= 180; i++){
+    let index = floor (map(i, 0, 180, 0, wave.length - 1))
+
+    let r = map(wave[index], -1, 1, 150, 350)
+
+    let x = r * sin (i);
+    let y = r * cos (i);
+    vertex (x, y)
+  }
+  endShape();
+
+  beginShape()
+  for(let i = 0; i<= 180; i++){
+    let index = floor (map(i, 0, 180, 0, wave.length - 1))
+
+    let r = map(wave[index], -1, 1, 150, 350)
+
+    let x = r * -sin (i);
+    let y = r * cos (i);
+    vertex (x, y)
+  }
+  endShape();
+
+
+
+}
+
+function mouseClicked() {
+  if (song.isPlaying()) {
+    song.pause()
+    fill(0)
+    noLoop();
+  } else {
+    song.play()
+    fill(0,0,255)
+    loop();
+  }
 }
